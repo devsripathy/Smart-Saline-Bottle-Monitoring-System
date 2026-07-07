789a86714643851f25886a2a114c3a8d60312c2a
devsripathy <dev.sripathy@gmail.com>
2026-07-07 12:07:13 +0530

    Remove unused methods and variables from IRSensor

--- firmware/node/include/IRSensor.h
+++ firmware/node/include/IRSensor.h
@@ -1,53 +1,14 @@
 #ifndef IR_SENSOR_H
 #define IR_SENSOR_H
 
-#include <Arduino.h>
-
 class IRSensor
 {
 public:
 
-    explicit IRSensor(uint8_t pin);
-
     bool begin();
 
-    void update();
+    bool isDripping();
 
-    bool dripDetected();
-
-    uint32_t getDripCount() const;
-
-    float getFlowRate();
-
-    bool noDrip();
-
-    bool sensorHealthy();
-
-    void resetCounter();
-
-private:
-
-    uint8_t _pin;
-
-    bool _lastState;
-
-    bool _dripEvent;
-
-    uint32_t _dripCount;
-
-    unsigned long _lastDripTime;
-
-    unsigned long _lastPulseTime;
-
-    unsigned long _windowStart;
-
-    uint32_t _windowCount;
-
-    static constexpr uint16_t DEBOUNCE_MS = 40;
-
-    static constexpr uint16_t FLOW_WINDOW_MS = 60000;
-
-    static constexpr uint16_t NO_DRIP_TIMEOUT_MS = 10000;
 };
 
 #endif

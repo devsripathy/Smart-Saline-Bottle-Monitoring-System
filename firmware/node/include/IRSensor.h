ec1eab0dc5d17c3a009b8083c6e0031a1a9c3088
devsripathy <dev.sripathy@gmail.com>
2026-07-07 14:22:06 +0530

    Add IRSensor class with flow detection methods

--- firmware/node/include/IRSensor.h
+++ firmware/node/include/IRSensor.h
@@ -1,14 +1,48 @@
 #ifndef IR_SENSOR_H
 #define IR_SENSOR_H
 
+#include <Arduino.h>
+
 class IRSensor
 {
 public:
 
+    explicit IRSensor(uint8_t pin);
+
     bool begin();
 
-    bool isDripping();
+    void update();
 
+    bool dripDetected();
+
+    uint32_t getDripCount() const;
+
+    float getFlowRate();
+
+    bool noDrip();
+
+    bool sensorHealthy();
+
+    void resetCounter();
+
+private:
+
+    uint8_t _pin;
+
+    bool _lastState;
+    bool _dripEvent;
+
+    uint32_t _dripCount;
+
+    unsigned long _lastDripTime;
+    unsigned long _lastPulseTime;
+
+    unsigned long _windowStart;
+    uint32_t _windowCount;
+
+    static constexpr uint16_t DEBOUNCE_MS = 40;
+    static constexpr uint32_t FLOW_WINDOW_MS = 60000;
+    static constexpr uint32_t NO_DRIP_TIMEOUT_MS = 10000;
 };
 
 #endif

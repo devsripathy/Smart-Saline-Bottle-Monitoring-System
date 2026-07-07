581bbde33c7c73ec2d62861e602ec7ac62b00f35
devsripathy <dev.sripathy@gmail.com>
2026-07-07 12:15:34 +0530

    Refactor sendPacket method and add constructor

--- firmware/node/include/LoRaManager.h
+++ firmware/node/include/LoRaManager.h
@@ -6,11 +6,44 @@
 class LoRaManager
 {
 public:
+    LoRaManager(
+        long frequency,
+        uint8_t ssPin,
+        uint8_t rstPin,
+        uint8_t dio0Pin);
 
     bool begin();
 
-    void sendPacket(String json);
+    bool sendPacket(
+        const String& deviceId,
+        float weight,
+        float percentage,
+        uint32_t dripCount,
+        float flowRate,
+        const String& status);
 
+    bool available();
+
+    String receivePacket();
+
+private:
+    long _frequency;
+
+    uint8_t _ssPin;
+    uint8_t _rstPin;
+    uint8_t _dio0Pin;
+
+    uint16_t _packetCounter;
+
+    String createPacket(
+        const String& deviceId,
+        float weight,
+        float percentage,
+        uint32_t dripCount,
+        float flowRate,
+        const String& status);
+
+    bool waitForAck(uint32_t timeout);
 };
 
 #endif

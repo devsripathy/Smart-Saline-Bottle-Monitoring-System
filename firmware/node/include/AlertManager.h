c3e04957e1b830426f49bd4d09eab77290ca652c
devsripathy <dev.sripathy@gmail.com>
2026-07-07 12:19:13 +0530

    Update AlertManager.h

--- firmware/node/include/AlertManager.h
+++ firmware/node/include/AlertManager.h
@@ -1,12 +1,46 @@
 #ifndef ALERT_MANAGER_H
 #define ALERT_MANAGER_H
 
+#include <Arduino.h>
+#include "SensorFusion.h"
+
 class AlertManager
 {
 public:
 
-    void update(float percentage,bool drip);
+    explicit AlertManager(uint8_t ledPin);
 
+    bool begin();
+
+    void update(SalineStatus status);
+
+    SalineStatus getCurrentAlert() const;
+
+private:
+
+    uint8_t _ledPin;
+
+    SalineStatus _currentAlert;
+
+    unsigned long _lastBlink;
+
+    bool _ledState;
+
+    void normal();
+
+    void lowSaline();
+
+    void emptyBottle();
+
+    void bottleRemoved();
+
+    void noDrip();
+
+    void tubeBlocked();
+
+    void sensorFailure();
+
+    void blink(uint16_t interval);
 };
 
 #endif

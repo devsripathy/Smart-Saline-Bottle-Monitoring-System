c5bc293c9a46b3abdf9f2a89a87c6b04366ff326
devsripathy <dev.sripathy@gmail.com>
2026-07-07 14:25:34 +0530

    Add HX711Sensor class for weight measurement

--- firmware/node/include/HX711Sensor.h
+++ firmware/node/include/HX711Sensor.h
@@ -1,16 +1,125 @@
 #ifndef HX711_SENSOR_H
 #define HX711_SENSOR_H
 
+#include <Arduino.h>
+#include <HX711.h>
+
 class HX711Sensor
 {
 public:
 
+    /*
+    ============================================================
+    Constructor
+    ============================================================
+    */
+
+    HX711Sensor(
+        uint8_t doutPin,
+        uint8_t sckPin,
+        float calibrationFactor,
+        float emptyWeight,
+        float fullWeight);
+
+    /*
+    ============================================================
+    Initialization
+    ============================================================
+    */
+
     bool begin();
+
+    /*
+    ============================================================
+    Sensor Operations
+    ============================================================
+    */
+
+    void tare();
 
     float getWeight();
 
+    float getFilteredWeight();
+
+    /*
+    ============================================================
+    Bottle Information
+    ============================================================
+    */
+
     float getPercentage();
 
+    bool bottlePresent();
+
+    bool isBottleEmpty();
+
+    bool isBottleLow();
+
+    /*
+    ============================================================
+    Calibration
+    ============================================================
+    */
+
+    void setCalibrationFactor(float factor);
+
+    float getCalibrationFactor() const;
+
+    /*
+    ============================================================
+    Diagnostics
+    ============================================================
+    */
+
+    bool sensorHealthy();
+
+private:
+
+    /*
+    ============================================================
+    HX711 Object
+    ============================================================
+    */
+
+    HX711 _scale;
+
+    /*
+    ============================================================
+    Configuration
+    ============================================================
+    */
+
+    uint8_t _doutPin;
+    uint8_t _sckPin;
+
+    float _calibrationFactor;
+
+    float _emptyWeight;
+    float _fullWeight;
+
+    /*
+    ============================================================
+    Moving Average Filter
+    ============================================================
+    */
+
+    static constexpr uint8_t FILTER_SIZE = 10;
+
+    float _samples[FILTER_SIZE];
+
+    uint8_t _sampleIndex;
+
+    bool _bufferFilled;
+
+    /*
+    ============================================================
+    Helper Functions
+    ============================================================
+    */
+
+    float applyMovingAverage(float value);
+
+    float constrainPercentage(float percent);
 };
 
 #endif
